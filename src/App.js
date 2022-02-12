6bfbb4b7d527014c56a749a9bf4c27e6b3e3e4bd
E. Peters <ericdpeters@gmail.com>
2022-02-12 12:39:26 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import GalvanicCouples from "./GalvanicCouples";
+import CssBaseline from "@mui/material/CssBaseline";
+
+export default function App() {
+    return (
+        <CssBaseline>
+            <div className="App">
+                <GalvanicCouples />
+            </div>
+        </CssBaseline>
+    );
+}
---
+++ src/GalvanicCouples.js
@@ -0,0 +1,201 @@
+import { Component } from "react";
+//import Select from "@mui/material/TextField";
+import FormControl from "@mui/material/FormControl";
+//import InputLabel from "@mui/material/InputLabel";
+//import MenuItem from "@mui/material/MenuItem";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+//import Card from "@mui/material/Card";
+//import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+const data = require("./data/galvanic_data.json");
+
+const matl_list = Object.keys(data.materials);
+
+function CoatingList(props) {
+    //do stuff
+    // get coating group for both materials
+    var grp1 = data["materials"][props.mat1]?.["group"] || "";
+    var grp2 = data["materials"][props.mat2]?.["group"] || "";
+    var pair_info = data["pairs"]?.[props.pair] || null;
+
+    // create array with lookup keys for the two finishes, based on sorted (A,B)
+    // pair key
+    var fin_keys = pair_info ? ["A.3.".concat(pair_info.finish_1), "A.3.".concat(pair_info.finish_2)] : null;
+    // get coating_list from data.surface_treatments based on keys in finishes
+    // var finishes = fin_keys ? [[data.surface_treatments[fin_keys[0]]], [data.surface_treatments[fin_keys[1]]]] : null;
+
+    // create object containing finish lists based on material group
+    // {props.pair[1] : finish_1 -> fin_key[finish_1] -> data.surface_treatments[fin_key],
+    //  props.pair[3] : finish_2 -> fin_key[finish_2] -> data.surface_treatments[fin_key]}
+    var finishes = pair_info
+        ? {
+              [props.pair[1]]: data.surface_treatments["A.3.".concat(pair_info.finish_1)],
+              [props.pair[3]]: data.surface_treatments["A.3.".concat(pair_info.finish_2)]
+          }
+        : null;
+    console.log(finishes);
+
+    return (
+        <Paper align="center" elevation={2}>
+            <Grid container spacing={2}>
+                <Grid item md>
+                    Approved Coatings: {props.mat1}
+                    <ul style={{ "text-align": "left" }}>
+                        <li style={{ "list-style": "none" }}>(Most effective)</li>
+                        {finishes ? finishes[grp1]["coating_list"].map((f, i) => <li key={i}>{f}</li>) : "..."}
+                        <li style={{ "list-style": "none" }}>(Least effective)</li>
+                    </ul>
+                </Grid>
+                <Grid item md>
+                    Approved Coatings: {props.mat2}
+                    <ul style={{ "text-align": "left" }}>
+                        <li style={{ "list-style": "none" }}>(Most effective)</li>
+                        {finishes ? finishes[grp2]["coating_list"].map((f, i) => <li key={i}>{f}</li>) : "..."}
+                        <li style={{ "list-style": "none" }}>(Least effective)</li>
+                    </ul>
+                </Grid>
+            </Grid>
+        </Paper>
+    );
+}
+
+function CoupleCard(props) {
+    // get info for chosen pair
+    var info = data.pairs[props.pair];
+    console.log(info);
+
+    return (
+        <Paper align="center" elevation={2}>
+            <table>
+                <tbody>
+                    <tr>
+                        <td align="center">Material 1: {props.mat1}</td>
+                        <td></td>
+                        <td align="center">Material 2: {props.mat2}</td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td align="center">
+                            Bare Metal Saltwater Compatibility:
+                            <br />
+                            {info?.rating_sea || "-"}
+                        </td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td align="center">
+                            Bare Metal Marine Environment Compatibility
+                            <br />
+                            {info?.rating_ma || "-"}
+                        </td>
+                        <td></td>
+                        <td align="center">
+                            Bare Metal Industrial Environment Compatibility
+                            <br />
+                            {info?.rating_ia || "-"}
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </Paper>
+    );
+}
+
+function MaterialList(props) {
+    const { mat_list } = props;
+    return mat_list.map((m) => (
+        <option key={m} value={m}>
+            {m}
+        </option>
+    ));
+}
+
+export default class GalvanicCouples extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { mat1: "", mat2: "", pair_key: "" };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleSubmit(e) {
+        e.preventDefault();
+        const { mat1, mat2 } = this.state;
+        if (mat1 === "" || mat2 === "") {
+            alert("Please select two materials");
+        }
+        // get coating group for both materials
+        const grp1 = data.materials[mat1].group;
+        const grp2 = data.materials[mat2].group;
+        const sg = [grp1, grp2].sort();
+
+        // create (A,B) key
+        const pair_key = "(".concat(sg[0], ",", sg[1], ")");
+        console.log(pair_key);
+        this.setState({ pair_key: pair_key });
+        console.log(this.state);
+    }
+
+    handleChange(e) {
+        const { name, value } = e.target;
+        var newState = this.state;
+        newState[name] = value;
+        this.setState(newState);
+    }
+
+    render() {
+        return (
+            <Box sx={{ bgcolor: "#D3D3D3" }}>
+                <Paper align="center" elevation={2}>
+                    <form onSubmit={this.handleSubmit}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={3}>
+                                <FormControl>
+                                    <label htmlFor="mat1-select">Material 1</label>
+                                    <select
+                                        id="mat1-select"
+                                        name="mat1"
+                                        label="Material 1"
+                                        value={this.state.mat1}
+                                        onChange={this.handleChange}>
+                                        <option key="blank" value="" disabled>
+                                            -
+                                        </option>
+                                        <MaterialList mat_list={matl_list} />
+                                    </select>
+                                </FormControl>
+                            </Grid>
+                            <Grid item xs={3}>
+                                <FormControl>
+                                    <label htmlFor="mat2-select">Material 2</label>
+                                    <select
+                                        id="mat2-select"
+                                        name="mat2"
+                                        label="Material 2"
+                                        value={this.state.mat2}
+                                        onChange={this.handleChange}>
+                                        <option key="blank" value="" disabled>
+                                            -
+                                        </option>
+                                        <MaterialList mat_list={matl_list} />
+                                    </select>
+                                </FormControl>
+                            </Grid>
+                            <Grid item>
+                                <Button type="submit" variant="contained">
+                                    Show Info
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </form>
+                </Paper>
+                <br />
+                <CoupleCard mat1={this.state.mat1} mat2={this.state.mat2} pair={this.state.pair_key} />
+                <br />
+                <CoatingList mat1={this.state.mat1} mat2={this.state.mat2} pair={this.state.pair_key} />
+            </Box>
+        );
+    }
+}
